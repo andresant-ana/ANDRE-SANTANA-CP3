bc61ce6adf60e659b3227094acc8246fcd54d82d
andresant-ana <rm551575@fiap.com.br>
2023-05-18 20:10:36 -0300

    Criada função validarSegundoNome no arquivo js

--- cp3.js
+++ cp3.js
@@ -11,3 +11,15 @@ function validarPrimeiroNome() {
         return true;
     }
 }
+
+function validarSegundoNome() {
+    var segundoNome = document.getElementById("segundo-nome");
+    var segundoNomeErro = document.getElementById("segundo-nome-erro");
+    if (segundoNome.value.length < 5 || segundoNome.value.trim() == "") {
+        segundoNome.classList.add("is-invalid");
+        return false;
+    } else {
+        segundoNome.classList.remove("is-invalid");
+        return true;
+    }
+}
