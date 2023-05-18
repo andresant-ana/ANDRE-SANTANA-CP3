1cf91ccb4a04b123711b201f28e7c75b2151dbeb
andresant-ana <rm551575@fiap.com.br>
2023-05-18 20:09:15 -0300

    Feito display: none na função de validação 1° nome

--- cp3.js
+++ cp3.js
@@ -3,9 +3,11 @@ function validarPrimeiroNome() {
     var primeiroNomeErro = document.getElementById("primeiro-nome-erro");
     if (primeiroNome.value.length < 5 || primeiroNome.value.trim() == "") {
         primeiroNome.classList.add("is-invalid");
+        primeiroNomeErro.classList.remove("d-none");
         return false;
     } else {
         primeiroNome.classList.remove("is-invalid");
+        primeiroNomeErro.classList.add("d-none");
         return true;
     }
 }
